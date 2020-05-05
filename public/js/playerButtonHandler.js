54c5a1dee2fc17f4235d78146fb3e76aa6dea7af
Jony Oinas <35299547+oinasjo@users.noreply.github.com>
2020-05-05 22:26:36 +0300

    Quickie

--- public/js/playerButtonHandler.js
+++ public/js/playerButtonHandler.js
@@ -23,78 +23,51 @@ let Randomizer = {
             switch(param) {
                 case Consts.MENU_1:
                     if(!this.MENU_1_ACTIVE) {
+                        this.MENU_1_ACTIVE = true;
                         arrow = document.querySelector('#arrow1');
-                        initialOffset = Randomizer.offSet(arrow);                   
-                        Randomizer.initAnimation(initialOffset, arrow.id, Consts.MENU_1);
-                    }
+                        Randomizer.initAnimation(arrow.id, Consts.MENU_1);
+                    } 
                 break;
                 case Consts.MENU_2:
                     if(!this.MENU_2_ACTIVE) {
+                        this.MENU_2_ACTIVE = true;
                         arrow = document.querySelector('#arrow2');
-                        initialOffset = Randomizer.offSet(arrow);                    
-                        Randomizer.initAnimation(initialOffset, arrow.id, Consts.MENU_2);
-                    }
+                        Randomizer.initAnimation(arrow.id, Consts.MENU_2);
+                    } 
                 break;
                 case Consts.MENU_3:
                     if(!this.MENU_3_ACTIVE) {
+                        this.MENU_3_ACTIVE = true;
                         arrow = document.querySelector('#arrow3');
-                        initialOffset = Randomizer.offSet(arrow);                   
-                        Randomizer.initAnimation(initialOffset, arrow.id, Consts.MENU_3);
-                    }
+                        Randomizer.initAnimation(arrow.id, Consts.MENU_3);
+                    } 
                 break;
             }
         }
     },
-    offSet: function (el) {
-	    var rect = el.getBoundingClientRect(),
-	    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-	    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-	    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
-    },
-    initAnimation: function (initialOffset, arrowId, menuId) {
-        console.log("[DEBUG] Randomizer start");
-        let configObject = {
-            elem: document.getElementById(arrowId),
-            pos: Randomizer.positionArray[0],
-            id: setInterval(frame, 10),
-            RANDOM_NUM: Math.floor(Math.random() * 20) + 7,
-            CURRENT_NUM: 0,
-            speed1: 5,
-            speed2: 6,
-            lastPosition: Randomizer.positionArray[0],
-            firstPosition: Randomizer.positionArray[0]
-        }
-        let elem = document.getElementById(arrowId);   
-        let pos = Randomizer.positionArray[0];
+    initAnimation: function (arrowId, menuId) {
+        let configObject = Randomizer.buildConfigObject(arrowId, menuId);
         let id = setInterval(frame, 10);
-        let RANDOM_NUM = Math.floor(Math.random() * 20) + 7;
-        let CURRENT_NUM = 0;
-        let speed1 = 5;
-        let speed2 = 6;
-        let lastPosition = Randomizer.positionArray[0];
-        let firstPosition = Randomizer.positionArray[0];
-        function frame() {
-            runAnimation(menuId);
+        function frame() {            
+            Randomizer.runAnimation(configObject, id);                
         }
-        console.log("[DEBUG] Randomizer stop");
     },
-    runAnimation: function(menuId) {
-        if(menuId === Consts.MENU_1) {
-            this.MENU_1_ACTIVE = true;
-            if(CURRENT_NUM <= RANDOM_NUM) {
-                if(lastPosition == Randomizer.positionArray[0]) {
-                    pos += speed1;
-                    elem.style.left = pos + "px"; 
-                    if(pos >= Randomizer.positionArray[1]) {
-                        lastPosition = Randomizer.positionArray[1];
-                        CURRENT_NUM++;
+    runAnimation: function(configObject, id) {
+        if(configObject.menuId === Consts.MENU_1) {
+            if(configObject.CURRENT_NUM <= configObject.RANDOM_NUM) {
+                if(configObject.lastPosition == Randomizer.positionArray[0]) {
+                    configObject.pos += configObject.speed1;
+                    configObject.elem.style.left = configObject.pos + "px"; 
+                    if(configObject.pos >= Randomizer.positionArray[1]) {
+                        configObject.lastPosition = Randomizer.positionArray[1];
+                        configObject.CURRENT_NUM++;
                     }
-                } else if(lastPosition == Randomizer.positionArray[1]) {
-                    pos -= speed1;
-                    elem.style.left = pos + "px"; 
-                    if(pos <= Randomizer.positionArray[0]) {
-                        lastPosition = Randomizer.positionArray[0];
-                        CURRENT_NUM++;
+                } else if(configObject.lastPosition == Randomizer.positionArray[1]) {
+                    configObject.pos -= configObject.speed1;
+                    configObject.elem.style.left = configObject.pos + "px"; 
+                    if(configObject.pos <= Randomizer.positionArray[0]) {
+                        configObject.lastPosition = Randomizer.positionArray[0];
+                        configObject.CURRENT_NUM++;
                     }
                 }
             } else {
@@ -102,46 +75,64 @@ let Randomizer = {
                 this.MENU_1_ACTIVE = false;
             }
         } else {
-            if(CURRENT_NUM <= RANDOM_NUM) {
-                if(lastPosition == Randomizer.positionArray[0]) {
-                    pos += speed2;
-                    elem.style.left = pos + "px"; 
-                    if(pos >= Randomizer.positionArray[1]) {
-                        lastPosition = Randomizer.positionArray[1];
-                        firstPosition = Randomizer.positionArray[0];
-                        CURRENT_NUM++;
+            if(configObject.CURRENT_NUM <= configObject.RANDOM_NUM) {
+                if(configObject.lastPosition == Randomizer.positionArray[0]) {
+                    configObject.pos += configObject.speed2;
+                    configObject.elem.style.left = configObject.pos + "px"; 
+                    if(configObject.pos >= Randomizer.positionArray[1]) {
+                        configObject.lastPosition = Randomizer.positionArray[1];
+                        configObject.firstPosition = Randomizer.positionArray[0];
+                        configObject.CURRENT_NUM++;
                     }
-                } else if(lastPosition == Randomizer.positionArray[1] && firstPosition == Randomizer.positionArray[0]) {
-                    pos += speed2;
-                    elem.style.left = pos + "px"; 
-                    if(pos >= Randomizer.positionArray[2]) {
-                        lastPosition = Randomizer.positionArray[2];
-                        firstPosition = Randomizer.positionArray[1];
-                        CURRENT_NUM++;
+                } else if(configObject.lastPosition == Randomizer.positionArray[1] && configObject.firstPosition == Randomizer.positionArray[0]) {
+                    configObject.pos += configObject.speed2;
+                    configObject.elem.style.left = configObject.pos + "px"; 
+                    if(configObject.pos >= Randomizer.positionArray[2]) {
+                        configObject.lastPosition = Randomizer.positionArray[2];
+                        configObject.firstPosition = Randomizer.positionArray[1];
+                        configObject.CURRENT_NUM++;
                     }
-                } else if(lastPosition == Randomizer.positionArray[2] && firstPosition == Randomizer.positionArray[1]) {
-                    pos -= speed2;
-                    elem.style.left = pos + "px"; 
-                    if(pos <= Randomizer.positionArray[1]) {
-                        lastPosition = Randomizer.positionArray[1];
-                        firstPosition = Randomizer.positionArray[2];
-                        CURRENT_NUM++;
+                } else if(configObject.lastPosition == Randomizer.positionArray[2] && configObject.firstPosition == Randomizer.positionArray[1]) {
+                    configObject.pos -= configObject.speed2;
+                    configObject.elem.style.left = configObject.pos + "px"; 
+                    if(configObject.pos <= Randomizer.positionArray[1]) {
+                        configObject.lastPosition = Randomizer.positionArray[1];
+                        configObject.firstPosition = Randomizer.positionArray[2];
+                        configObject.CURRENT_NUM++;
                     }
-                } else if(lastPosition == Randomizer.positionArray[1] && firstPosition == Randomizer.positionArray[2]) {
-                    pos -= speed2;
-                    elem.style.left = pos + "px"; 
-                    if(pos <= Randomizer.positionArray[0]) {
-                        lastPosition = Randomizer.positionArray[0];
-                        firstPosition = Randomizer.positionArray[1];
-                        CURRENT_NUM++;
+                } else if(configObject.lastPosition == Randomizer.positionArray[1] && configObject.firstPosition == Randomizer.positionArray[2]) {
+                    configObject.pos -= configObject.speed2;
+                    configObject.elem.style.left = configObject.pos + "px"; 
+                    if(configObject.pos <= Randomizer.positionArray[0]) {
+                        configObject.lastPosition = Randomizer.positionArray[0];
+                        configObject.firstPosition = Randomizer.positionArray[1];
+                        configObject.CURRENT_NUM++;
                     }
                 } 
             } else {
                 clearInterval(id);
+                if(configObject.menuId == Consts.MENU_2) {
+                    this.MENU_2_ACTIVE = false;
+                } else {
+                    this.MENU_3_ACTIVE = false;
+                }
             }
         }  
     },
     pushToPositionArray: function(positionX) {
         this.positionArray.push(positionX);
+    },
+    buildConfigObject: function(id, menuId) {
+        return {
+            pos: Randomizer.positionArray[0],
+            elem: document.getElementById(id),
+            RANDOM_NUM: Math.floor(Math.random() * 20) + 7,
+            CURRENT_NUM: 0,
+            speed1: 8,
+            speed2: 10,
+            lastPosition: Randomizer.positionArray[0],
+            firstPosition: Randomizer.positionArray[0],
+            menuId: menuId
+        }
     }
 }
